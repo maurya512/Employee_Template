da1b786b8a0316f34cddc468b9836c16abdb4e6d
Maurya Patel <mauryapatel@Chaitrangis-MacBook-Pro.local>
2020-08-16 18:20:35 -0400

    created a file that will work the logic of getting and setting the info of an engineer

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee{
+    constructor(name,id,email,github) {
+        super(id,name,email);
+        this.id = "Engineer";
+        this.github = github;
+    }
+    // create a method to set the employee's role as an engineer
+    getRole() {
+        return this.id;
+    }
+    // create a method to get the employer's github user name
+    getGithub() {
+        return this.github;
+    }
+}
+
+// export the modules
+module.exports = Engineer;
