2aa28c09d822a95d284990182ddf60cce87385bb
Maurya Patel <mauryapatel@Chaitrangis-MacBook-Pro.local>
2020-08-16 16:54:12 -0400

    created a file based on the unit test requirements that were needed to be checked

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,36 @@
 // TODO: Write code to define and export the Employee class
+// create a class called employee
+class Employee {
+    // create a constructor that will hold the atrributes of an employee
+    constructor(name, id, email){
+        // instantiate the properties 
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.title = "Employee"
+        
+    }
+
+    // a method to return the employee's name
+    getName(){
+        return this.name;
+    }
+
+    // a method to return the employee's id
+    getId(){
+        return this.id;
+    }
+
+    // a method to return the employee's email
+    getEmail(){
+        return this.email;
+    }
+
+    // a method to return the employee's role
+    getRole(){
+        return this.title;
+    }
+}
+
+
+module.exports = Employee
