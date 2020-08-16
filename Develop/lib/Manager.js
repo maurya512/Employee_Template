a14c8539494e56280ee8c484eabfe1b497f6957f
Maurya Patel <mauryapatel@Chaitrangis-MacBook-Pro.local>
2020-08-16 17:58:30 -0400

    created a file that works on the logic of getting and setting manager's info

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+// inherited from employee class 
+const Employee = require("./Employee");
+
+// create a class that extends from employee class
+class Manager extends Employee {
+    // create a constructor that takes the manager's arguments
+    constructor(name,id,email,officeNumber) {
+        super(name, id, email);
+        this.id = "Manager";
+        this.officeNumber = officeNumber;
+    }
+    // create a method that returns the manager's job title
+    getRole () {
+        return this.id;
+    }
+    // create a method that returns the manager's office number
+    getOfficeNumber() {
+        return this.officeNumber;
+    }
+}
+
+// export the class Manager to be used at different locations
+module.exports = Manager;
