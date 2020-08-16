31f88f17cb34e4c9dd855f0f84a5768f882a2510
Maurya Patel <mauryapatel@Chaitrangis-MacBook-Pro.local>
2020-08-16 18:10:56 -0400

    added the logic behind getting and setting the intern's info

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Intern extends Employee{
+    constructor(id,name,email,school) {
+        super(id, name, email);
+        this.id = "Intern";
+        this.school = school;
+    }
+
+    // create a method to get the intern's role at the company
+    getRole() {
+        return this.id;
+    }
+
+    // create a method to get the intern's school 
+    getSchool() {
+        return this.school;
+    }
+}
+
+module.exports = Intern;
